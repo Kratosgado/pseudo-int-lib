2bfd156e118e826562d0cf6b458fe2a4d4b9dcb4
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-07-06 12:01:22 +0000

    successful error response

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,45 @@ pub mod parser;
 pub mod std;
 mod utils;
 
+use constants::error_handler::PseudoError;
 pub use evaluator::evaluator::Evaluator;
 pub use lexer::lexer::Lexer;
 pub use parser::parser::Parser;
 
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+extern "C" {
+
+    // #[wasm_bindgen(constructor)]
+    // fn new(message: &str) -> WebPseudoError;
+}
 
 #[wasm_bindgen]
-pub fn interpret(inputs: &str)-> Result<JsValue, JsError> {
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+impl Into<JsValue> for PseudoError {
+    fn into(self) -> JsValue {
+        JsValue::from(self.to_string())
+    }
+}
+
+#[no_mangle]
+#[wasm_bindgen]
+pub fn interpret(inputs: &str) -> Result<JsValue, PseudoError> {
     let mut lexer = Lexer::new(inputs);
-    let tokens = lexer.tokenize().map_err(|e| JsError::from(e))?;
+    let tokens = lexer.tokenize().map_err(|_e| _e)?;
 
     let mut parser = Parser::new(tokens);
-    let parsed_token = parser.parse().map_err(|e| JsError::from(e))?;
+    let parsed_token = parser.parse()?;
 
     let mut evaluator = Evaluator::new(parsed_token);
 
     match evaluator.evaluate() {
         Ok(output) => Ok(JsValue::from_str(output.as_str())),
-        Err(e) => Err(JsError::from(e) )   
+        Err(e) => Err(e),
     }
 }
